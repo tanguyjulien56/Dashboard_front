a7183ddd7a072de764c5f6a83b17436e1920c28b
Julien TANGUY <tanguyjulien@hotmail.fr>
2024-11-07 11:30:44 +0100

    feat(sidebar): replace setting button in navbar

--- src/app/theme/header/header.component.ts
+++ src/app/theme/header/header.component.ts
@@ -1,24 +1,42 @@
-import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  HostBinding,
+  inject,
+  Input,
+  OnDestroy,
+  Output,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import screenfull from 'screenfull';
 
+import { CommonModule } from '@angular/common';
+import { AppSettings, SettingsService } from '@core';
+import { CustomizerComponent } from '@theme/customizer/customizer.component';
+import { filter, Subscription } from 'rxjs';
 import { BrandingComponent } from '../widgets/branding.component';
 import { NotificationComponent } from '../widgets/notification.component';
 import { TranslateComponent } from '../widgets/translate.component';
 import { UserComponent } from '../widgets/user.component';
+import { MatSidenav, MatSidenavContent } from '@angular/material/sidenav';
+import { NavigationEnd, Router } from '@angular/router';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
+const MOBILE_MEDIAQUERY = 'screen and (max-width: 599px)';
+const TABLET_MEDIAQUERY = 'screen and (min-width: 600px) and (max-width: 959px)';
+const MONITOR_MEDIAQUERY = 'screen and (min-width: 960px)';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
-  host: {
-    class: 'matero-header',
-  },
+  styleUrls: ['./header.component.scss'],
   encapsulation: ViewEncapsulation.None,
   standalone: true,
   imports: [
+    CommonModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
@@ -26,18 +44,110 @@ import { UserComponent } from '../widgets/user.component';
     NotificationComponent,
     TranslateComponent,
     UserComponent,
+    CustomizerComponent,
   ],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnDestroy {
+  @ViewChild('sidenav', { static: true }) sidenav!: MatSidenav;
+  @ViewChild('content', { static: true }) content!: MatSidenavContent;
   @Input() showToggle = true;
   @Input() showBranding = false;
 
   @Output() toggleSidenav = new EventEmitter<void>();
   @Output() toggleSidenavNotice = new EventEmitter<void>();
 
+  private readonly breakpointObserver = inject(BreakpointObserver);
+  private readonly router = inject(Router);
+  private readonly settings = inject(SettingsService);
+
+  options = this.settings.options;
+  openCustomizer = false;
+
+  get themeColor() {
+    return this.settings.themeColor;
+  }
+
+  get isOver() {
+    return this.isMobileScreen;
+  }
+  private isMobileScreen = false;
   toggleFullscreen() {
     if (screenfull.isEnabled) {
       screenfull.toggle();
     }
+  }
+  @HostBinding('class.matero-content-width-fix')
+  get contentWidthFix() {
+    return (
+      this.isContentWidthFixed &&
+      this.options.navPos === 'side' &&
+      this.options.sidenavOpened &&
+      !this.isOver
+    );
+  }
+
+  private isContentWidthFixed = true;
+
+  @HostBinding('class.matero-sidenav-collapsed-fix')
+  get collapsedWidthFix() {
+    return (
+      this.isCollapsedWidthFixed &&
+      (this.options.navPos === 'top' || (this.options.sidenavOpened && this.isOver))
+    );
+  }
+
+  private isCollapsedWidthFixed = false;
+
+  private layoutChangesSubscription = Subscription.EMPTY;
+  constructor() {
+    this.layoutChangesSubscription = this.breakpointObserver
+      .observe([MOBILE_MEDIAQUERY, TABLET_MEDIAQUERY, MONITOR_MEDIAQUERY])
+      .subscribe(state => {
+        // SidenavOpened must be reset true when layout changes
+        this.options.sidenavOpened = true;
+
+        this.isMobileScreen = state.breakpoints[MOBILE_MEDIAQUERY];
+        this.options.sidenavCollapsed = state.breakpoints[TABLET_MEDIAQUERY];
+        this.isContentWidthFixed = state.breakpoints[MONITOR_MEDIAQUERY];
+      });
+
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(e => {
+      if (this.isOver) {
+        this.sidenav.close();
+      }
+      this.content.scrollTo({ top: 0 });
+    });
+  }
+
+  ngOnDestroy() {
+    this.layoutChangesSubscription.unsubscribe();
+  }
+
+  toggleCollapsed() {
+    this.isContentWidthFixed = false;
+    this.options.sidenavCollapsed = !this.options.sidenavCollapsed;
+    this.resetCollapsedState();
+  }
+
+  // TODO: Trigger when transition end
+  resetCollapsedState(timer = 400) {
+    setTimeout(() => this.settings.setOptions(this.options), timer);
+  }
+
+  onSidenavClosedStart() {
+    this.isContentWidthFixed = false;
+  }
+
+  onSidenavOpenedChange(isOpened: boolean) {
+    this.isCollapsedWidthFixed = !this.isOver;
+    this.options.sidenavOpened = isOpened;
+    this.settings.setOptions(this.options);
+  }
+
+  updateOptions(options: AppSettings) {
+    this.options = options;
+    this.settings.setOptions(options);
+    this.settings.setDirection();
+    this.settings.setTheme();
   }
 }
